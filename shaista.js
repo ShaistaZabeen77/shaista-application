4cf0f9cd1416a1440a6c76b8770241fb72028569
Shaista Zabeen <118909515+ShaistaZabeen77@users.noreply.github.com>
2022-12-14 10:32:59 +0530

    Create shaista.js

---
+++ shaista.js
@@ -0,0 +1,10 @@
+function resume(){
+    firstname=document.getElementById("firstname").value
+    console.log(firstname);
+let lastname;
+lastname=document.getElementById("lastname").value  
+console.log(lastname);
+document.getElementById("name").innerHTML=firstname+lastname;
+    
+    
+    }
